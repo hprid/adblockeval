38f303ed8f2433b69ccf4429328856d42f9e8337
Henning Pridöhl <henning.pridoehl@hprid.de>
2018-09-23 15:08:30 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='adblockeval',
+    version='0.1-dev',
+    packages=[''],
+    url='https://github.com/hprid/adblockeval',
+    license='MIT',
+    author='Henning Pridöhl',
+    author_email='henning.pridoehl@hprid.de',
+    description='Evaluates URLs against AdBlock rules efficiently to check which rules would block them, if any.'
+)
