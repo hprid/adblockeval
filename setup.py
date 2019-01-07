14c20ed597e0bb0c81b2851db9ad2d28366eb822
Henning Pridöhl <henning.pridoehl@hprid.de>
2019-01-07 22:37:49 +0100

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,2 @@
-from distutils.core import setup
-
-setup(
-    name='adblockeval',
-    version='0.1-dev',
-    packages=['adblockeval'],
-    url='https://github.com/hprid/adblockeval',
-    license='MIT',
-    author='Henning Pridöhl',
-    author_email='henning.pridoehl@hprid.de',
-    description='Evaluates URLs against AdBlock rules efficiently to check which rules would block them, if any.'
-)
+import setuptools
+setuptools.setup()
